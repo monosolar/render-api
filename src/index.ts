6ffbefad2ead2934757a4b1ae360b3ddb1033b97
Yauheni Kasenko <yauheni.kasenko@instinctools.com>
2025-04-15 13:43:44 +0200

    first

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import cors from 'cors';
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({ message: 'Hello from Express with CORS and TypeScript!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
